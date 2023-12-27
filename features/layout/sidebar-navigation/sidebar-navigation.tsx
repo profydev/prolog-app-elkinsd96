19e7b540e83449dd29fdd0a3dbba43eab219381b
elkinsd96 <elkinsd96@gmail.com>
2023-12-27 10:24:36 -0700

    Rename Button to UnstyledButton

--- features/layout/sidebar-navigation/sidebar-navigation.tsx
+++ features/layout/sidebar-navigation/sidebar-navigation.tsx
@@ -5,7 +5,7 @@ import classNames from "classnames";
 import { NavigationContext } from "./navigation-context";
 import { MenuItemButton } from "./menu-item-button";
 import { MenuItemLink } from "./menu-item-link";
-import { Button } from "@features/ui";
+import { UnstyledButton } from "@features/ui";
 import styles from "./sidebar-navigation.module.scss";
 
 const menuItems = [
@@ -53,7 +53,7 @@ export function SidebarNavigation() {
               isSidebarCollapsed && styles.isCollapsed,
             )}
           />
-          <Button
+          <UnstyledButton
             onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
             className={styles.menuButton}
           >
@@ -63,7 +63,7 @@ export function SidebarNavigation() {
               alt={isMobileMenuOpen ? "close menu" : "open menu"}
               className={styles.menuIcon}
             />
-          </Button>
+          </UnstyledButton>
         </header>
         <div
           className={classNames(
--- features/ui/button/unstyledButton.tsx
+++ features/ui/button/unstyledButton.tsx
@@ -2,7 +2,7 @@ import { ButtonHTMLAttributes } from "react";
 import classNames from "classnames";
 import styles from "./button.module.scss";
 
-export function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
+export function UnstyledButton(props: ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button {...props} className={classNames(styles.button, props.className)} />
   );
