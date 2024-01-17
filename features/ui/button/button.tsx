27d0b451556ec835b6ed3dc166efd0c6bd76edce
elkinsd96 <elkinsd96@gmail.com>
2024-01-17 14:33:07 -0700

    Create button component and button styles.

--- features/ui/button/button.tsx
+++ features/ui/button/button.tsx
@@ -1,6 +1,51 @@
 import { ButtonHTMLAttributes } from "react";
 import { UnstyledButton } from "./unstyledButton";
+import styles from "./button.module.scss";
+import classNames from "classnames";
 
-export function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
-  return <UnstyledButton {...props} />;
+export enum ButtonSize {
+  small = "sm",
+  medium = "md",
+  large = "lg",
+  xlarge = "xl",
+}
+
+export enum ButtonColor {
+  Primary = "primary",
+  Secondary = "secondary",
+  Gray = "gray",
+  Error = "error",
+}
+
+export enum ButtonVariant {
+  Default = "default",
+  Empty = "empty",
+  iconOnly = "iconOnly",
+}
+
+type ButtonProps = {
+  size?: ButtonSize;
+  color?: ButtonColor;
+  variant?: ButtonVariant;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function Button({
+  className,
+  size = ButtonSize.medium,
+  color = ButtonColor.Primary,
+  variant = ButtonVariant.Default,
+  ...props
+}: ButtonProps) {
+  return (
+    <UnstyledButton
+      className={classNames(
+        styles.button,
+        styles[size],
+        styles[color],
+        styles[variant],
+        className,
+      )}
+      {...props}
+    />
+  );
 }
